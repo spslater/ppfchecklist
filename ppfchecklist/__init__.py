304fa18043c903354ddf04afaf1a9f62ffec5cae
Sean Slater <seanslater@whatno.io>
2024-11-13 02:51:28 -0500

    Add protective check for running from main
    Add the if `__name__` check for calling `app()`.

--- ppfchecklist/__init__.py
+++ ppfchecklist/__init__.py
@@ -215,4 +215,5 @@ logging.basicConfig(
     handlers=handler_list,
 )
 
-app.run(host="0.0.0.0", port=port, debug=debug)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port, debug=debug)
